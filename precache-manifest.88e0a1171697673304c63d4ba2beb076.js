6cfd938a465c954dc2844fdeebc1793ebfb436ee
JulesBlm <julesblom@gmail.com>
2018-12-28 16:51:56 +0100

    Updates

---
+++ precache-manifest.88e0a1171697673304c63d4ba2beb076.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d71dd35534fe6e7c7514",
+    "url": "/static/js/main.d71dd355.chunk.js"
+  },
+  {
+    "revision": "35aaae7e315ff67d1110",
+    "url": "/static/js/1.35aaae7e.chunk.js"
+  },
+  {
+    "revision": "d71dd35534fe6e7c7514",
+    "url": "/static/css/main.6a6fde3c.chunk.css"
+  },
+  {
+    "revision": "65f57bc21a06cb9802a2441a976ba566",
+    "url": "/index.html"
+  }
+];
