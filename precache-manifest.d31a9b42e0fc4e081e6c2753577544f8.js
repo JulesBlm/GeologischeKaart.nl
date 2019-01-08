a2ecea77cae505d5f09bc38716ab7301a90ecf08
JulesBlm <julesblom@gmail.com>
2019-01-08 20:38:52 +0100

    Updates

---
+++ precache-manifest.d31a9b42e0fc4e081e6c2753577544f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "08b61d7d936d1cbe5fbd",
+    "url": "/static/js/main.08b61d7d.chunk.js"
+  },
+  {
+    "revision": "9ba9bf0ebffbcae5d167",
+    "url": "/static/js/1.9ba9bf0e.chunk.js"
+  },
+  {
+    "revision": "08b61d7d936d1cbe5fbd",
+    "url": "/static/css/main.f91fcbdf.chunk.css"
+  },
+  {
+    "revision": "58b09c2478cb45caed1390b699100220",
+    "url": "/index.html"
+  }
+];
